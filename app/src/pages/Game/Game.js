7742295a8101e3004c9445ada91a221049ad920d
Nilo <nilobdrumond@gmail.com>
2021-11-23 19:41:17 -0300

    match highlighting

--- app/src/pages/Game/Game.js
+++ app/src/pages/Game/Game.js
@@ -1,150 +1,191 @@
-import React, { useEffect, useContext, useState, useRef } from 'react'
+import React, {
+  useEffect,
+  useContext,
+  useState,
+  useRef,
+  useCallback,
+} from "react";
 
-import socketIOClient from 'socket.io-client'
+import socketIOClient from "socket.io-client";
 
-import history from '../../history'
+import history from "../../history";
 
-import { Context } from '../../Context/UserProvider'
+import {Context} from "../../Context/UserProvider";
 
-import kirby from '../../images/kirby.png'
+import kirby from "../../images/kirby.png";
 
-import InvitePlayer from '../InvitePlayer/InvitePlayer'
-import PlayerInfo from './PlayerInfo'
-import Word from './Word'
+import InvitePlayer from "../InvitePlayer/InvitePlayer";
+import PlayerInfo from "./PlayerInfo";
+import Word from "./Word";
 
-require("dotenv").config()
+require("dotenv").config();
 
-const SOCKET_ENDPOINT = process.env.SOCKET_ENDPOINT || "http://localhost:3000"
+const SOCKET_ENDPOINT = process.env.SOCKET_ENDPOINT || "http://localhost:3000";
 
 const Game = () => {
+  const {match, clientId, player} = useContext(Context);
 
-    const { match, clientId, player } = useContext(Context)
+  const [gameStarted, setGameStarted] = useState(false);
 
-    const [gameStarted, setGameStarted] = useState(false)
+  const [gameMatch, setGameMatch] = useState({});
 
-    const [gameMatch, setGameMatch] = useState({})
+  const [gamePlayer, setGamePlayer] = useState({});
 
-    const [gamePlayer, setGamePlayer] = useState({})
+  const socketRef = useRef();
 
-    const socketRef = useRef()
+  const typedRef = useRef("");
 
-    var typed = ""
+  const handlePress = useCallback((e) => {
+    const key = e.key;
 
-    const handlePress = (e) => {
-        const key = e.key
+    const isSmallCaseLetter = (k) => {
+      return k >= "a" && k <= "z";
+    };
 
-        const isSmallCaseLetter = (k) => {
-            return k >= 'a' && k <= 'z'
+    const isUpperCaseLetter = (k) => {
+      return k >= "A" && k <= "Z";
+    };
+
+    if (!isSmallCaseLetter(key) && !isUpperCaseLetter(key)) return;
+
+    const words = document.getElementsByClassName("board-word");
+
+    var match = false;
+
+    typedRef.current += key.toLowerCase();
+
+    for (let index = 0; index < words.length; index++) {
+      const w = words[index].getAttribute("value");
+      if (w.length < typedRef.current.length) continue;
+      else if (w.length === typedRef.current.length) {
+        if (w.toLowerCase() === typedRef.current.toLowerCase()) {
+          socketRef.current.emit("word_finished", words[index].id);
+          typedRef.current = "";
+          break;
         }
-
-        const isUpperCaseLetter = (k) => {
-            return k >= 'A' && k <= 'Z'
-        }
-
-        if(!isSmallCaseLetter(key) && !isUpperCaseLetter(key)) return
-        
-        const words = document.getElementsByClassName("board-word")
-
-        var match = false
-
-        typed += key
-
-        for(let index = 0 ; index < words.length ; index++){
-            const w = words[index].getAttribute("value")
-            if(w.length < typed.length) continue
-            else if(w.length === typed.length){
-                if(w === typed){
-                    socketRef.current.emit("word_finished", words[index].id)
-                    typed=""
-                    break
-                }
-            }
-            else if(w.substr(0, typed.length) === typed){
-                match = true
-            }
-        }
-
-        if(!match){
-            typed = ""
-        }
+      } else if (
+        w.substr(0, typedRef.current.length).toLowerCase() ===
+        typedRef.current.toLowerCase()
+      ) {
+        match = true;
+      }
     }
 
-    useEffect(() => {
-        if(!clientId) history.push("/")
+    if (!match) {
+      typedRef.current = "";
+    }
+  }, []);
 
-        window.addEventListener("keypress", handlePress)
+  useEffect(() => {
+    if (!clientId) history.push("/");
 
-        const socket = socketIOClient(`${SOCKET_ENDPOINT}/match/${match.id}`, {
-            auth: {
-                playerId: player.id
-            }   
-        })
+    window.addEventListener("keypress", handlePress);
 
-        socket.on("start_match", (match) => {
-            setGameMatch(match)
-            setGamePlayer(match.players[player.id])
-            setGameStarted(true)
-        })
+    const socket = socketIOClient(`${SOCKET_ENDPOINT}/match/${match.id}`, {
+      auth: {
+        playerId: player.id,
+      },
+    });
 
-        socket.on("update_player", (newPlayerState) => {
-            setGamePlayer(newPlayerState)
-        })
+    socket.on("start_match", (match) => {
+      setGameMatch(match);
+      setGamePlayer(match.players[player.id]);
+      setGameStarted(true);
+    });
 
-        socket.on("other_player_update", (newPlayer) => {
-            setGameMatch((previous) => {
-                return {...previous, players: {...previous.players, [newPlayer.id]: {...previous.players[newPlayer.id], ...newPlayer}}}
-            })
-        })
+    socket.on("update_player", (newPlayerState) => {
+      setGamePlayer(newPlayerState);
+    });
 
-        socket.on("message", msg => {
-            alert(msg.text)
-        }) 
+    socket.on("other_player_update", (newPlayer) => {
+      setGameMatch((previous) => {
+        return {
+          ...previous,
+          players: {
+            ...previous.players,
+            [newPlayer.id]: {...previous.players[newPlayer.id], ...newPlayer},
+          },
+        };
+      });
+    });
 
-        socketRef.current = socket
-    },[])
+    socket.on("message", (msg) => {
+      alert(msg.text);
+    });
 
-    return(
-        <div className="full-screen-container">
-            { gameStarted ? 
-            <div className="game-window">
-                <div className="header-game-window">
-                    <PlayerInfo
-                        username={gamePlayer.user.nickname}
-                        hp={gamePlayer.hp}
-                    />
-                    {
-                        Object.values(gameMatch.players).map((currentPlayer) => {
-                            return (
-                                player.created_at === currentPlayer.created_at ? <div key="div"></div> : 
-                                <PlayerInfo
-                                    username={currentPlayer.user.nickname}
-                                    hp={currentPlayer.hp}
-                                    key={currentPlayer.created_at}
-                                    float="right"
-                                />
-                            )
-                        })
-                    }
-                </div>
-                <div className="main-game-window">
-                    <div className="main-game-window-left">
-                        <img src={kirby} alt="kirby-fofo" style={{width: "100%", height: "100%"}}/>
-                    </div>
-                    <div className="main-game-window-right" style={{position: "relative"}}>
-                        {
-                            gamePlayer.board.words.map((word) => {
-                                return(
-                                    < Word word={word} y={gamePlayer.board.yMap[word.id]} matchCount={2}/>
-                                )
-                            })
-                        }                   
-                    </div>
-                </div>
-                <div className="footer-game-window"></div>
+    socketRef.current = socket;
+
+    return () => {
+      socket.off("message");
+      socket.off("update_player");
+      socket.off("other_player_update");
+      socket.off("start_match");
+      window.removeEventListener("keypress", handlePress);
+      socket.disconnect();
+    };
+  }, [clientId, handlePress, player.id, match.id]);
+
+  const getMatchCount = useCallback((word) => {
+    if (word.length < typedRef.current) return 0;
+    const w = word.toLowerCase();
+    return w.substr(0, typedRef.current.length).toLowerCase() ===
+      typedRef.current.toLowerCase()
+      ? typedRef.current.length
+      : 0;
+  }, []);
+
+  return (
+    <div className="full-screen-container">
+      {gameStarted ? (
+        <div className="game-window">
+          <div className="header-game-window">
+            <PlayerInfo
+              username={gamePlayer.user.nickname}
+              hp={gamePlayer.hp}
+            />
+            {Object.values(gameMatch.players).map((currentPlayer) => {
+              return player.created_at === currentPlayer.created_at ? (
+                <div key="div"></div>
+              ) : (
+                <PlayerInfo
+                  username={currentPlayer.user.nickname}
+                  hp={currentPlayer.hp}
+                  key={currentPlayer.created_at}
+                  float="right"
+                />
+              );
+            })}
+          </div>
+          <div className="main-game-window">
+            <div className="main-game-window-left">
+              <img
+                src={kirby}
+                alt="kirby-fofo"
+                style={{width: "100%", height: "100%"}}
+              />
             </div>
-            : <InvitePlayer /> }
+            <div
+              className="main-game-window-right"
+              style={{position: "relative"}}
+            >
+              {gamePlayer.board.words.map((word) => {
+                return (
+                  <Word
+                    word={word}
+                    y={gamePlayer.board.yMap[word.id]}
+                    matchCount={getMatchCount}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className="footer-game-window"></div>
         </div>
-    )
-}
+      ) : (
+        <InvitePlayer />
+      )}
+    </div>
+  );
+};
 
-export default Game
+export default Game;
--- app/src/pages/Game/Word.js
+++ app/src/pages/Game/Word.js
@@ -1,27 +1,30 @@
-import React from "react"
+import React from "react";
 
 const Word = ({word, y, matchCount}) => {
-    return (
-        <div>
-            <span style={{
-                position: "absolute",
-                left: `${100 - word.position}%`,
-                top:`${y}%`,
-                fontSize: "2rem",
-                transition: "left 95ms",
-            }}
-                className="board-word"
-                key={word.id}
-                id={word.id}
-                value={word.word}
-            >
-                <span>
-                    <span style={{color: "#67da86"}}>{word.word.substr(0,matchCount)}</span>
-                    {word.word.substr(matchCount)}
-                </span>
-            </span>
-        </div>
-    )
-}
+  return (
+    <div>
+      <span
+        style={{
+          position: "absolute",
+          left: `${100 - word.position}%`,
+          top: `${y}%`,
+          fontSize: "2rem",
+          transition: "left 95ms",
+        }}
+        className="board-word"
+        key={word.id}
+        id={word.id}
+        value={word.word}
+      >
+        <span>
+          <span style={{color: "#67da86"}}>
+            {word.word.substr(0, matchCount(word.word))}
+          </span>
+          {word.word.substr(matchCount(word.word))}
+        </span>
+      </span>
+    </div>
+  );
+};
 
-export default Word
+export default Word;
